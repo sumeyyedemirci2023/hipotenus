c41ac71ff5e596c3f07a759c76027ffbc5eed205
sumeyyedemirci2023 <145452558+sumeyyedemirci2023@users.noreply.github.com>
2023-12-18 11:26:22 +0300

    Add files via upload

---
+++ hipotenushesapla/hipotenushesapla/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace hipotenushesapla
+{
+    partial class lblXcvp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.lblXcevap = new System.Windows.Forms.Label();
+            this.txtSayi1 = new System.Windows.Forms.TextBox();
+            this.txtSayi2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(47, 12);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(88, 63);
+            this.btnHesapla.TabIndex = 0;
+            this.btnHesapla.Text = "button1";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // lblXcevap
+            // 
+            this.lblXcevap.AutoSize = true;
+            this.lblXcevap.Location = new System.Drawing.Point(302, 37);
+            this.lblXcevap.Name = "lblXcevap";
+            this.lblXcevap.Size = new System.Drawing.Size(35, 13);
+            this.lblXcevap.TabIndex = 1;
+            this.lblXcevap.Text = "label1";
+            // 
+            // txtSayi1
+            // 
+            this.txtSayi1.Location = new System.Drawing.Point(158, 12);
+            this.txtSayi1.Name = "txtSayi1";
+            this.txtSayi1.Size = new System.Drawing.Size(100, 20);
+            this.txtSayi1.TabIndex = 2;
+            // 
+            // txtSayi2
+            // 
+            this.txtSayi2.Location = new System.Drawing.Point(158, 55);
+            this.txtSayi2.Name = "txtSayi2";
+            this.txtSayi2.Size = new System.Drawing.Size(100, 20);
+            this.txtSayi2.TabIndex = 3;
+            // 
+            // lblXcvp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 198);
+            this.Controls.Add(this.txtSayi2);
+            this.Controls.Add(this.txtSayi1);
+            this.Controls.Add(this.lblXcevap);
+            this.Controls.Add(this.btnHesapla);
+            this.Name = "lblXcvp";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.Label lblXcevap;
+        private System.Windows.Forms.TextBox txtSayi1;
+        private System.Windows.Forms.TextBox txtSayi2;
+    }
+}
+
---
+++ hipotenushesapla/hipotenushesapla/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hipotenushesapla
+{
+    public partial class lblXcvp : Form
+    {
+        public lblXcvp()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            int sayi1=Convert.ToInt32(txtSayi1.Text);
+            int sayi2 = Convert.ToInt32(txtSayi2.Text);
+            int hesap =(sayi1 *sayi1 + sayi2*sayi2);
+            double sonuc = Math.Sqrt(hesap);
+            lblXcevap.Text = sonuc.ToString();
+        }
+    }
+}
